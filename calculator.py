a8c0048a0785e4d21ae592d4fc7d2b391860aa24
broni673 <broni673@student.otago.ac.nz>
2024-04-13 16:40:38 +1200

    function decomposition

--- calculator.py
+++ calculator.py
@@ -8,24 +8,34 @@ dict = {
     "I": 1
 }
 
-user_input = ""
+def validate(input):
+    r = 0
+    l = 0
 
+    # Count runs and run lengths of letters
+    # Invalid if any run exceeds 3
+    # If run of higher letter follows run of lower letter, invalid if run exceeds 1
+
+    return True
+
+def convert(input):
+    l = 0
+    n = 0
+    while l < len(user_input):
+        if l < len(user_input) - 1: # Check all digits which have a following digit
+            if dict[user_input[l]] >= dict[user_input[l+1]]:
+                n += dict[user_input[l]] # If next digit is lower, add value
+            else:
+                n -= dict[user_input[l]] # If next digit is higher, take value
+        else:
+            n += dict[user_input[l]] # Add value of final digit
+        l += 1
+    return n
+    
 while user_input == "":
     
     user_input = input("Enter numeral or Q: ")
-    
-    if user_input != "Q":
-        l = 0
-        n = 0
-        while l < len(user_input):
-            if l < len(user_input) - 1:
-                if dict[user_input[l]] >= dict[user_input[l+1]]:
-                    n += dict[user_input[l]]
-                else:
-                    n -= dict[user_input[l]]
-            else:
-                n += dict[user_input[l]]
-            l += 1
-        
-        print("{} = {}".format(user_input, n))
+
+    if user_input != "Q" and validate(user_input):
+        print("{} = {}".format(user_input, convert(user_input)))
         user_input = ""
