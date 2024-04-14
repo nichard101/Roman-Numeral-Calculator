232e5e86c0df858c77418c014a660f9885b612d7
broni673 <broni673@student.otago.ac.nz>
2024-04-14 16:59:38 +1200

    romantodecimal tweaks

--- romantodecimal.py
+++ romantodecimal.py
@@ -1,12 +1,8 @@
-import math
-
 MAX_ADDITIVE = 3
 MAX_SUBTRACTIVE = 1
 
-last_letter = ""
-
-roman = ["M", "D", "C", "L", "X", "V", "I"]
-decimal = [1000, 500, 100, 50, 10, 5, 1]
+ROMAN = ["M", "D", "C", "L", "X", "V", "I"]
+DECIMAL = [1000, 500, 100, 50, 10, 5, 1]
 
 def R2D(letter):
     """
@@ -15,9 +11,12 @@ def R2D(letter):
     if letter == None:
         return 0
     else:
-        return decimal[roman.index(letter)]
+        return DECIMAL[ROMAN.index(letter)]
 
 def isEven(num):
+    """
+    Returns True if even, False if odd
+    """
     if num % 2 == 0:
         return True
     else:
@@ -38,15 +37,21 @@ def getDigitsDifference(A, B):
     return db-da
 
 def getFirstDigit(letter):
+    """
+    Returns first integer digit of input Roman numeral
+    """
     num = R2D(letter)
     while num >= 10:
         num /= 10
     return num
 
 def isSameDigit(letterA,letterB):
+    """
+    Returns True if the first digits match, False otherwise
+    """
     return getFirstDigit(letterA) == getFirstDigit(letterB)
 
-def validateSlice(A, B=None, C=None): # UP DOWN
+def findOperation(A, B=None, C=None): # UP DOWN
     """
     A       A>B=C   A>B>C   A>C>B
     A+B     A=B>C   
@@ -90,6 +95,9 @@ def validateSlice(A, B=None, C=None): # UP DOWN
         return 0
 
 def countLetters(input, letter):
+    """
+    Returns number of occurrences of input letter within input string
+    """
     n = 0
     for d in input:
         if letter == d:
@@ -97,11 +105,17 @@ def countLetters(input, letter):
     return n
 
 def validateAllLetters(input):
+    """
+    Checks if all letters in input string are valid Roman numerals
+    """
     for letter in input:
-        if letter not in roman:
+        if letter not in ROMAN:
             raise Exception("Letter {} not valid".format(letter))
 
 def validateRuns(input):
+    """
+    Checks that input string does not contain more than a set amount of the same character in succession
+    """
     run = 1
     prev_letter = ""
     for letter in input:       
@@ -116,38 +130,49 @@ def validateRuns(input):
         prev_letter = letter
         
 def processInput(user_input):
+    """
+    Takes string input and converts it to integer number output
+    """
+
     output = 0
+
     validateAllLetters(user_input)
     validateRuns(user_input)
+
     while len(user_input) > 0:
+
         a = user_input[0]
         b = None
         c = None
+
         if len(user_input) >= 2:
             b = user_input[1]
         if len(user_input) >= 3:
             c = user_input[2]
 
-        n = validateSlice(a, b, c)
+        n = findOperation(a, b, c)
+
         if n == 0:
             output += R2D(a)
             user_input = user_input[1:]
+
         elif n == 1:
             output -= R2D(a)
             output += R2D(b)
             user_input = user_input[2:]
+
     return output
+
+
+#############################################################
 
 user_input = ""
 
 while user_input == "":
     user_input = input("Enter numeral or Q: ")
-    #print(user_input)
-
     if user_input != "Q":
         try:
             print("{} = {}".format(user_input, processInput(user_input)))
         except Exception as err:
             print(err.args)
-
     user_input = ""
